8e19780d8950ced1eeb24b620da31a9509216cf2
Kim <kn222gp@student.lnu.se>
2016-11-16 17:01:19 +0100

    RED

--- HigherLower/ConsoleView.java
+++ HigherLower/ConsoleView.java
@@ -1,6 +1,12 @@
 package HigherLower;
 
+import java.io.PrintStream;
+
 public class ConsoleView {
+
+	public ConsoleView(PrintStream printStream) {
+		// TODO Auto-generated constructor stub
+	}
 
 	public Object showMenu() {
 		// TODO Auto-generated method stub
---
+++ Tests/ConsoleViewTest.java
@@ -0,0 +1,31 @@
+package Tests;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import HigherLower.ConsoleView;
+
+public class ConsoleViewTest {
+
+	private ConsoleView sut;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void shouldShowMenu() {
+		
+		PrintStream printStream = mock(PrintStream.class);
+		sut = new ConsoleView(printStream);
+		sut.showMenu();
+		
+		verify(printStream).println();
+	
+	}
+
+}
