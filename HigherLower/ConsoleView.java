396c7d66606750d4817ea83c8280d98c1ae4f92b
Kim <kn222gp@student.lnu.se>
2016-11-16 17:22:47 +0100

    REFACTOR

--- HigherLower/ConsoleView.java
+++ HigherLower/ConsoleView.java
@@ -5,13 +5,14 @@ import java.io.PrintStream;
 public class ConsoleView {
 
 	private PrintStream output;
+	public final static String MENU = "Higher or Lower!\nIs the next card (h)igher or (l)ower than the current card?\nPress \"q\" to (q)uit";
 
 	public ConsoleView(PrintStream printStream) {
 		this.output = printStream;
 	}
 
 	public void showMenu() {
-		output.println("Higher or Lower!\nIs the next card (h)igher or (l)ower than the current card?\nPress \"q\" to (q)uit");
+		output.println(MENU);
 	}
 
 }
--- Tests/ConsoleViewTest.java
+++ Tests/ConsoleViewTest.java
@@ -1,12 +1,10 @@
 package Tests;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import java.io.PrintStream;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import HigherLower.ConsoleView;
 
 public class ConsoleViewTest {
@@ -24,7 +22,7 @@ public class ConsoleViewTest {
 		sut = new ConsoleView(printStream);
 		sut.showMenu();
 		
-		verify(printStream).println("Higher or Lower!\nIs the next card (h)igher or (l)ower than the current card?\nPress \"q\" to (q)uit");
+		verify(printStream).println(ConsoleView.MENU);
 	
 	}
 
